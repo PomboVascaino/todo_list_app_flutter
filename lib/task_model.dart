e4d6f8bb8cad3033e91e036092842ae5734d6a75
PomboVascaino <alvesarthur186@gmail.com>
2025-08-28 14:38:37 -0300

    criando task_model

---
+++ lib/task_model.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+class Task {
+  String title;
+  bool isDone;
+
+  Task({required this.title, this.isDone = false});
+
+  Map<String, dynamic> toMap() {
+    return {'title': title, 'isDone': isDone};
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(title: map['title'] ?? '', isDone: map['isDone'] ?? false);
+  }
+
+  String toJson() => jsonEncode(toMap());
+
+  factory Task.fromJson(String source) => Task.fromMap(jsonDecode(source));
+}
